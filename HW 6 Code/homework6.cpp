40b89936b58a0ec8908fbb5a4fa823aba57f9222
Kalia <kaliadhall@gmail.com>
2019-11-19 10:26:12 -0600

    after first checkpoint

--- HW 6 Code/homework6.cpp
+++ HW 6 Code/homework6.cpp
@@ -53,6 +53,7 @@ public:
 
 	//Getters
 	string get_original() const;
+	bool get_valid() const;
 	vector<Token> get_tokenized() const;
 
 	//Setter
@@ -60,8 +61,7 @@ public:
 
 	//Others
 	void display() const;
-    string displayVector() const;
-
+    string displayExpression() const;
 
 private:
 	//Data members
@@ -69,6 +69,7 @@ private:
 	vector<Token> tokenized;
 	vector<Token> postfix;
 	bool valid;
+	bool isValid();
 	Exp_type type;
 	vector<Token> tokenize(string s);
 };
@@ -83,21 +84,21 @@ public:
 
 	//Getters
 	string get_original() const;
+	bool get_valid() const;
 	vector<Expression> get_parsed() const;
 
 	//Setter
 	void set(string& s);
 
 	//Others
-	void display() const;
-    string displayVector() const;
-
+    string displaySequence() const;
 
 private:
 	//Data members
 	string original;
 	vector<Expression> parsed;
 	bool valid;
+	bool isValid();
 	vector<Expression> parse(string s);
 };
 
@@ -119,40 +120,28 @@ Token::Token(string s)
 //Helpers
 bool Token::isCharacter(char c)
 {
-        int ascii = int(c);
-        return ((ascii >= 65 && ascii <=90) || (ascii >= 97 && ascii <=122));
+	int ascii = int(c);
+	return ((ascii >= 65 && ascii <=90) || (ascii >= 97 && ascii <=122));
 }
 
 bool Token::isNumber(string s)
 {
-        if (s[0] == '0')
-        {
-                return false;
-        }
-        for (int i = 0; i < int(s.length()); i++)
-        {
-        if (isdigit(s[i]) == false)
-                {
-                        return false;
-                }
-        }
+	if (s[0] == '0') { return false; }
+	for (int i = 0; i < int(s.length()); i++)
+	{
+		if (isdigit(s[i]) == false) { return false; }
+	}
     return true;
 }
 
 bool Token::isIdentifier(string s)
 {
-        if (!isCharacter(s[0]))
-        {
-                return false;
-        }
-        for (int i = 1; i < int(s.length()); i++)
-        {
-                if (!( isCharacter(s[i]) || isNumber(string(1, s[i])) ))
-                {
-                        return false;
-                }
-        }
-        return true;
+	if (!isCharacter(s[0])) { return false; }
+	for (int i = 1; i < int(s.length()); i++)
+	{
+		if (!( isCharacter(s[i]) || isNumber(string(1, s[i])) )) { return false; }
+	}
+	return true;
 }
 
 //Getters
@@ -177,34 +166,13 @@ void Token::set(string s)
 	token = s;
 	priority = -1;
 
-        if(token == "(")
-        {
-                type = OpenBrace;
-        }
-        else if(token == ")")
-        {
-                type = CloseBrace;
-        }
-        else if(token == "=")
-        {
-                type = EQ;
-        }
-        else if(token == "+" || token == "-" || token == "*" || token == "/")
-        {
-                type = OP;
-        }
-        else if(isNumber(token))
-        {
-                type = INT;
-        }
-        else if(isIdentifier(token))
-        {
-                type = ID;
-        }
-        else
-        {
-                type = INVALID;
-        }
+	if(token == "(") { type = OpenBrace; }
+	else if(token == ")") { type = CloseBrace; }
+	else if(token == "=") { type = EQ; }
+	else if(token == "+" || token == "-" || token == "*" || token == "/") { type = OP; }
+	else if(isNumber(token)) { type = INT; }
+	else if(isIdentifier(token)) { type = ID; }
+	else { type = INVALID; }
 }
 
 //Others
@@ -217,44 +185,20 @@ int Token::value() const
 		str >>num;
 		return num;
 	}
-	else if(type == ID)
-	{
-		return -1;
-	}
+	else if(type == ID) { return -1; }
 	return -2;
 }
 
 void Token::display() const
 {
 	//type based on num Token_type {ID, INT, OP, EQ, OpenBrace, CloseBrace, INVALID}
-	if(type == 0)
-	{
-		cout << "type = " << "ID" << endl;
-	}
-    if(type == 1)
-    {
-            cout << "type = " << "INT" << endl;
-    }
-    if(type == 2)
-    {
-            cout << "type = " << "OP" << endl;
-    }
-    if(type == 3)
-    {
-            cout << "type = " << "EQ" << endl;
-    }
-    if(type == 4)
-    {
-            cout << "type = " << "OpenBrace" << endl;
-    }
-    else if(type == 5)
-    {
-            cout << "type = " << "CloseBrace" << endl;
-    }
-	else if (type == 6)
-	{
-		cout << "type = " << "INVALID" << endl;
-	}
+	if(type == 0) { cout << "type = " << "ID" << endl; }
+	if(type == 1) { cout << "type = " << "INT" << endl; }
+	if(type == 2) { cout << "type = " << "OP" << endl; }
+	if(type == 3) { cout << "type = " << "EQ" << endl; }
+	if(type == 4) { cout << "type = " << "OpenBrace" << endl; }
+	else if(type == 5) { cout << "type = " << "CloseBrace" << endl; }
+	else if (type == 6) { cout << "type = " << "INVALID" << endl; }
 	cout << "token = " << get_token()  << " (value is "<<  value() << ")" << endl;
 	cout << "priority = " << get_priority() << endl;
     cout << endl;
@@ -282,6 +226,11 @@ string Expression::get_original() const
 	return original;
 }
 
+bool Expression::get_valid() const
+{
+	return valid;
+}
+
 vector<Token> Expression::get_tokenized() const
 {
 	return tokenized;
@@ -292,8 +241,18 @@ void Expression::set(string& s)
 {
 	original = s;
 	tokenized = tokenize(s);
+	// assignment, arithmetic, illegal
 	type = illegal;
-	valid = 0;
+	valid = isValid();
+}
+
+bool Expression::isValid()
+{
+	for (int i=0; i<int(tokenized.size()); i++) 
+	{
+		if (tokenized[i].get_type() == 6) { return false; }
+	}
+	return true;
 }
 
 //Others
@@ -302,13 +261,13 @@ vector<Token> Expression::tokenize(string s)
 	vector<Token> list;
 	string tkn = "";
 	// for each character in the string
-	for(int i=0; i<= int(s.length()); i++)
-	{
+	for(int i=0; i<int(s.length()); i++)
+	{	
 		string chr = string(1, s[i]);
 		// if space, discard, close previous token if open
 		if (chr == " ")
 		{
-               		if (tkn != "")
+            if (tkn != "")
 			{
 				Token token(tkn);
 				list.push_back(token);
@@ -316,27 +275,24 @@ vector<Token> Expression::tokenize(string s)
 			}
 		}
 		// else if single-char type, tokenize, close previous token if open
-               	else if (chr == "(" || chr == ")" || chr == "=" || chr == "+" || chr == "-" || chr == "*" || chr == "/") 
+        else if (chr == "(" || chr == ")" || chr == "=" || chr == "+" || chr == "-" || chr == "*" || chr == "/") 
 		{
 			if (tkn != "")
 			{
-                        	Token token(tkn);
+                Token token(tkn);
 				list.push_back(token);
 				tkn = "";
 			}
-                    	Token token(chr);
-                    	list.push_back(token);
+			Token token(chr);
+			list.push_back(token);
 		}
-                // else add to open token
-                else
-		{
-			tkn = tkn + chr;
-		}
+		// else add to open token
+		else { tkn = tkn + chr; }
 	}
 	if (tkn != "")
 	{
-              	Token token(tkn);
-               	list.push_back(token);
+		Token token(tkn);
+		list.push_back(token);
 	}
 	return list;
 }
@@ -344,34 +300,25 @@ vector<Token> Expression::tokenize(string s)
 void Expression::display() const
 {
 	cout << "original = " << original << endl;
-	cout << "tokenized = " << displayVector() << endl;
+	cout << "tokenized = " << displayExpression() << endl;
 	cout << "number of tokens = " << tokenized.size() << endl;
 	cout << "postfix =  " << endl;
-	cout << "valid = false" << endl;
-    cout << endl;
+	cout << "valid = " << valid << endl;
 	//type based off enum Exp_type {assignment, arithmetic, illegal}
-	if(type == 0)
-    {
-            cout << "type = " << "assignment" << endl;
-    }
-    if(type == 1)
-    {
-            cout << "type = " << "arithmetic" << endl;
-    }
-    if(type == 2)
-    {
-            cout << "type = " << "illegal" << endl;
-	}
+	if(type == 0) { cout << "type = " << "assignment" << endl; }
+    if(type == 1) { cout << "type = " << "arithmetic" << endl; }
+    if(type == 2) { cout << "type = " << "illegal"    << endl; }
+    cout << endl;
 }
 
-string Expression::displayVector() const
+string Expression::displayExpression() const
 {
 	string display = "";
 	for(int i=0; i<int(tokenized.size()); i++)
 	{
-          	display = display + tokenized[i].get_token() + ";";
-        }
-        return display;
+		display = display + tokenized[i].get_token();
+	}
+	return display;
 }
 
 
@@ -395,6 +342,11 @@ string Sequence::get_original() const
 	return original;
 }
 
+bool Sequence::get_valid() const
+{
+	return valid;
+}
+
 vector<Expression> Sequence::get_parsed() const
 {
 	return parsed;
@@ -405,55 +357,50 @@ void Sequence::set(string& s)
 {
 	original = s;
 	parsed = parse(s);
-	valid = 0;
+	valid = isValid();
+}
+
+bool Sequence::isValid() {
+	if (original.length() > 0) 
+	{
+		if (original.back() != ';') { return false; }
+	} 
+	else { return true; }
+
+	vector<Expression> seq = get_parsed();
+	for(int i=0; i<int(seq.size()); i++)
+	{
+		if (seq[i].get_original() == "") { return false; }
+        if (! seq[i].get_valid()) { return false; }
+    }
+    return true;
 }
 
 //Others
 vector<Expression> Sequence::parse(string s)
 {
+    vector<Expression> list;
     string delim = ";";
     size_t pos = 0;
-    string token;
+    string exp;
     while ((pos = s.find(delim)) != string::npos) 
     {
-        token = s.substr(0, pos);
-        cout << token << endl;
+        exp = s.substr(0, pos);
+        Expression exp_obj = exp;
+        list.push_back(exp_obj);
         s.erase(0, pos + delim.length());
     }
-    cout << s << endl;
+    return list;
 }
 
-void Sequence::display() const
-{
-/*	cout << "original = " << original << endl;
-	cout << "parsed = " << displayVector() << endl;
-	cout << "number of tokens = " << parsed.size() << endl;
-	cout << "postfix =  " << endl;
-	cout << "valid = false" << endl;
-    cout << endl;
-	//type based off enum Exp_type {assignment, arithmetic, illegal}
-	if(type == 0)
-    {
-            cout << "type = " << "assignment" << endl;
-    }
-    if(type == 1)
-    {
-            cout << "type = " << "arithmetic" << endl;
-    }
-    if(type == 2)
-    {
-            cout << "type = " << "illegal" << endl;
-	}
-*/}
-
-string Sequence::displayVector() const
+string Sequence::displaySequence() const
 {
 	string display = "";
 	for(int i=0; i<int(parsed.size()); i++)
 	{
-          	display = display + parsed[i].displayVector() + ";";
-        }
-        return display;
+    	display = display + parsed[i].displayExpression() + ";";
+    }
+    return display;
 }
 
 
@@ -486,22 +433,45 @@ int main()
 	Expression exp4("1$%5+dfv*2@-@7===14 g");
 	exp4.display();
 
+	const string start  = "=== expression evaluation program starts ===";
+	const string end  = "=== expression evaluation program ends ===";
 
-    const string start  = "=== expression evaluation program starts ===";
-    const string end  = "=== expression evaluation program ends ===";
+	cout << endl << start << endl;
 
-    string input;
-    cout << "input:";
-    cin >> input;
-
-    Sequence seq1 = input;
-    cout << "seq1:" << seq1.get_original() << endl;
-
-    string action;
-    cout << "action:";
-    cin >> action;
-    cout << endl;
-
+	bool loop = true;
+	string input;
+	string action;
+	Sequence seq1;
+	while (loop) 
+	{
+		while (input == "") 
+		{
+			cout << "input:";
+			cin >> input;
+		}
+		string oldInput = seq1.get_original();
+		if (action == "c") { seq1 = seq1.get_original() + input; } 
+		else { seq1 = input; }	  
+		bool valid = seq1.get_valid();
+		if (!valid) 
+		{ 
+			cout << "Sequence is invalid." << endl; 
+			seq1 = oldInput;
+			input = "";
+		}
+		while (valid) 
+		{
+			cout << "action:";
+			cin >> action; 
+			if (action == "=") { cout << "evaluate (sequence: " << seq1.displaySequence() << ")" << endl; }
+			else if (action == ">") { cout << "display prefix expression" << endl; }
+			else if (action == "<") { cout << "display postfix expression" << endl; }
+			else if (action == "f") { cout << "display parenthesized expression" << endl; }
+			else if (action == "c") { input = ""; break; } 	
+			else if (action == "s") { input = ""; break; } 	
+			else if (action == "q") { loop = false; break; } 	
+		}
+	}
+	cout << end << endl;
 	return 0;
 }
-
